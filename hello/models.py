3e373ffe75f45d81e01d0b8e3e5426372e3adb2f
YutaroTakei <yutarotakei@icloud.com>
2020-02-22 11:00:55 +0900

    修正

--- hello/models.py
+++ hello/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Message(models.Model):
-    objects = None
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='message_owner')
     content = models.TextField(max_length=1000)
     pub_date = models.DateTimeField(auto_now_add=True)
